73c033b35f84000b27260b2ff164d338c95b0aeb
gabriel8programmer <gabriel8github@gmail.com>
2024-02-09 20:25:30 -0300

    exercises

---
+++ exercicio_002/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$name = "Gustavo";
+$lastName = "Otavio";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 2</title>
+</head>
+<body>
+    
+    <h1><?= $name . " " . $lastName ?></h1>
+    
+</body>
+</html>
---
+++ exercicio_003/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$n1 = 10;
+$n2 = 20;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 3</title>
+</head>
+<body>
+    <h1><?= $n1 . " + " . $n2 . " = " . $n1+$n2 ?></h1>
+</body>
+</html>
